abb9800fe847b622fe3598ad5478e10eb41fc6f3
si-shakil <sishakil850@gmail.com>
2022-07-30 22:35:28 +0600

    login page added

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,5 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./Components/Login";
 function App() {
-  return <div>Login</div>;
+  return (
+    <div className="flex justify-center items-center h-[100vh]">
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
