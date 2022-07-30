5b6af1a2922453001a94bf2b176b41c1f6707285
si-shakil <sishakil850@gmail.com>
2022-07-30 22:53:27 +0600

    register page added

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,11 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./Components/Login";
+import Register from "./Components/Register";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <div className="flex justify-center items-center h-[100vh]">
       <Routes>
         <Route path="/" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
       </Routes>
+      <ToastContainer />
     </div>
   );
 }
